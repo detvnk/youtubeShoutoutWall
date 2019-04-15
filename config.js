857a7ea1569ec3596f708870d06c68a15a165733
T-4NK <38303736+detvnk@users.noreply.github.com>
2019-04-15 12:05:36 +1000

    Added workaround for Custom Quote API restriction

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
 var CONFIG = {
-	"nightbotToken" : "xxx",
-	"googleApiKeys" : ["xxx"],
+
+	// Update the below keys as required
+	"nightbotToken" : "xxx", // Add your !quote token from nightbot custom quote API here
+	"googleApiKeys" : ["xxx"], // Add your google API key/s here
+	"nightbotPrefix" : "zzz", // Change this to align with whatever prefix you've set in your !wall command
+
+	// Customise the size of your wall here
 	"numberOfChannels" : 12,
 	"numberOfRows" : 2
 };
