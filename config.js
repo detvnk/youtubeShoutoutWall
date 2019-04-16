be4b92c9544a5854d5846ae0c6686b955a3d0e06
T-4NK <38303736+detvnk@users.noreply.github.com>
2019-04-16 17:15:33 +1000

    Fixed channelID string prefix replacement bug

--- config.js
+++ config.js
@@ -3,7 +3,10 @@ var CONFIG = {
 	// Update the below keys as required
 	"nightbotToken" : "xxx", // Add your !quote token from nightbot custom quote API here
 	"googleApiKeys" : ["xxx"], // Add your google API key/s here
-	"nightbotPrefix" : "zzz", // Change this to align with whatever prefix you've set in your !wall command
+	"nightbotPrefix" : /zzz/gi, // Change zzz to align with whatever prefix you've set in your !wall command
+
+	// Add channelID to use google API for Live Chat rather than Nightbot
+	"channelID" : "Uxxx", // Enter your youtube channelID here
 
 	// Customise the size of your wall here
 	"numberOfChannels" : 12,
